0b059d5005532de214f5192e2bf3a69b0c222e52
EwertonLRamos <elres.ewerton@gmail.com>
2022-01-05 11:16:29 -0300

    Adicionando arquivo para entender o conceito de classes

--- IntroducaoAoCsharpE.NET/Projeto/Program.cs
+++ IntroducaoAoCsharpE.NET/Projeto/Program.cs
@@ -7,11 +7,23 @@ namespace Projeto
     {
         static void Main(string[] args)
         {
+            //Testando Namespaces
             var classe1 = new Classe1();
             var classe2 = new Classe2();
             var classe3 = new Namespace2.Classe3();
             var classe4 = new Namespace2.Classe4();
-            
+
+            //Testando Classe
+            Pessoa pessoa1 = new Pessoa();
+            pessoa1.Nome = "Ewerton";
+            pessoa1.CPF = "123.456.789-10";
+            pessoa1.Idade = 21;
+
+            Pessoa pessoa2 = new Pessoa();
+            pessoa2.Nome = "Irineu";
+            pessoa2.CPF = "987.654.321-00";
+            pessoa2.Idade = 18;
+
             Console.WriteLine("Hello World!");
         }
     }
