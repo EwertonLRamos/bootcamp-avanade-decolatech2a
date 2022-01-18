e58f37d3c71549ae71e9af3ece08e4527c0a8931
EwertonLRamos <elres.ewerton@gmail.com>
2022-01-18 13:47:11 -0300

    Iniciando curso de POO

---
+++ ProgramandoComOOComCSharp/Abastracao/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Abastracao.Models;
+
+namespace Abastracao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa("Ewerton", 21);
+
+            p1.Apresentar();
+        }
+    }
+}
+
