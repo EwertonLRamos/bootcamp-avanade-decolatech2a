890e28f4b3e092efd01b77a7d8c14f3f9ce9a9de
EwertonLRamos <elres.ewerton@gmail.com>
2022-01-18 13:59:50 -0300

    Trabalhando e utilizando arquivos

---
+++ ProgramandoComOOComCSharp/TrabalhandoComArquivos/UtilizandoArquivos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UtilizandoArquivos.Helper;
+
+namespace UtilizandoArquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var caminho = "C:\\Users\\Ewerton Lucas\\source\\repos\\BootcampAvanade\\ProgramandoComOOComCSharp\\TrabalhandoComArquivos";
+            var caminhoPathCombine = Path.Combine(caminho, "Arquivo Teste.txt");
+            var novoCaminho = Path.Combine(caminho, "Pasta Teste 2", "Arquivo Teste Stream.txt");
+            var caminhoCopia = Path.Combine(caminho, "Arquivo Teste Copia.txt");
+
+            var listaString = new List<string> {"Linha1", "Linha2", "Linha3"};
+            var listaStringContinuacao = new List<string> {"Linha4", "Linha5", "Linha6"};
+            
+            FileHelper helper = new FileHelper();
+
+            //helper.ListarDiretorios(caminho);
+            //helper.ListarArquivosDiretorios(caminho);
+            //helper.CriarDiretorio(caminhoPathCombine);
+            //helper.ApagarDiretorio(caminhoPathCombine, true);
+            //helper.CriarArquivotexto(caminhoPathCombine, "Olá arquivo texto");
+            //helper.CriarArquivotextoStream(caminhoPathCombine, listaString); 
+            //helper.AdicionarArquivotextoStream(caminhoPathCombine, listaStringContinuacao);
+            //helper.LerArquivoStream(caminhoPathCombine);
+            //helper.MoverArquivos(caminhoPathCombine, novoCaminho, false);
+            //helper.CopiarArquivo(caminhoPathCombine, caminhoCopia, false);
+            helper.DeletarArquivo(caminhoCopia);
+        }
+    }
+}
