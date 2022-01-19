f9618577276976bab56d5327fbd6e9cd461f066b
EwertonLRamos <elres.ewerton@gmail.com>
2022-01-19 01:06:18 -0300

    Projeto exemplo de Eventos

---
+++ ConstrPropDelegEventsEmDotNET/Eventos/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Eventos.Models;
+
+namespace Eventos
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Matematica math = new Matematica(10, 20);
+            math.Somar();
+        }
+    }
+}
